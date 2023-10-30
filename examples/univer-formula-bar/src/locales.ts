4425d339f4d5ce76134f7fa72de756ddd2df7b0b
Sg <suica@users.noreply.github.com>
2023-10-30 22:00:07 +0800

    chore(formula): init

---
+++ examples/univer-formula-bar/src/locales.ts
@@ -0,0 +1,25 @@
+import DocPluginZhCN from '@univerjs/base-docs/locale/zh-CN';
+import SheetPluginZhCN from '@univerjs/base-sheets/locale/zh-CN';
+import BaseUiZhCN from '@univerjs/base-ui/locale/zh-CN';
+import DeisgnZH from '@univerjs/design/locale/zh-CN';
+import SheetDebuggerPluginZhCN from '@univerjs/sheets-plugin-debugger/locale/zh-CN';
+import FindPluginZhCN from '@univerjs/sheets-plugin-find/locale/zh-CN';
+import ImportXlsxPluginZhCN from '@univerjs/sheets-plugin-import-xlsx/locale/zh-CN';
+import NumberfmtPluginZhCN from '@univerjs/sheets-plugin-numfmt/locale/zh-CN';
+import DocUIPluginZhCN from '@univerjs/ui-plugin-docs/locale/zh-CN';
+import SheetUIPluginZhCN from '@univerjs/ui-plugin-sheets/locale/zh-CN';
+
+export const locales = {
+    zhCN: {
+        ...DocPluginZhCN,
+        ...SheetPluginZhCN,
+        ...FindPluginZhCN,
+        ...ImportXlsxPluginZhCN,
+        ...NumberfmtPluginZhCN,
+        ...DocUIPluginZhCN,
+        ...SheetUIPluginZhCN,
+        ...SheetDebuggerPluginZhCN,
+        ...BaseUiZhCN,
+        ...DeisgnZH,
+    },
+};
---
+++ examples/univer-formula-bar/src/main.tsx
@@ -0,0 +1,57 @@
+import { DocPlugin } from '@univerjs/base-docs';
+import { RenderEngine } from '@univerjs/base-render';
+import { SheetPlugin } from '@univerjs/base-sheets';
+import { UIPlugin } from '@univerjs/base-ui';
+import { DEFAULT_WORKBOOK_DATA_DEMO } from '@univerjs/common-plugin-data';
+import { LocaleType, Univer } from '@univerjs/core';
+import { greenTheme } from '@univerjs/design';
+import { DebuggerPlugin } from '@univerjs/sheets-plugin-debugger';
+// import { FindPlugin } from '@univerjs/sheets-plugin-find';
+// import { DEFAULT_FORMULA_DATA_DEMO, FormulaPlugin } from '@univerjs/sheets-plugin-formula';
+// import { ImagePlugin } from '@univerjs/sheets-plugin-image';
+// import { ImportXlsxPlugin } from '@univerjs/sheets-plugin-import-xlsx';
+// import { NumfmtPlugin } from '@univerjs/sheets-plugin-numfmt';
+import { SheetUIPlugin } from '@univerjs/ui-plugin-sheets';
+
+import { locales } from './locales';
+
+// univer
+const univer = new Univer({
+    theme: greenTheme,
+    locale: LocaleType.EN_US,
+    locales,
+});
+
+// core plugins
+univer.registerPlugin(DocPlugin, {
+    hasScroll: false,
+});
+// univer.registerPlugin(DocUIPlugin);
+univer.registerPlugin(RenderEngine);
+univer.registerPlugin(UIPlugin, {
+    container: 'univer-container',
+    header: true,
+    toolbar: true,
+    footer: true,
+});
+univer.registerPlugin(SheetPlugin);
+univer.registerPlugin(SheetUIPlugin);
+
+// sheet feature plugins
+univer.registerPlugin(DebuggerPlugin);
+// univer.registerPlugin(NumfmtPlugin);
+// univer.registerPlugin(FindPlugin);
+// univer.registerPlugin(ImportXlsxPlugin);
+// univer.registerPlugin(ImagePlugin);
+
+// create univer sheet instance
+univer.createUniverSheet(DEFAULT_WORKBOOK_DATA_DEMO);
+
+// use for console test
+declare global {
+    interface Window {
+        univer?: Univer;
+    }
+}
+
+window.univer = univer;
